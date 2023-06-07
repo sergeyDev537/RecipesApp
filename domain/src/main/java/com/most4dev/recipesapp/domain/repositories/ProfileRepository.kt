ec9f365ae73c80f996147a961b82e90b2f09844e
Sergey <sergiksavonow82@gmail.com>
2023-06-07 19:58:07 +0300

    Revert "Feature/domain layer"

--- domain/src/main/java/com/most4dev/recipesapp/domain/repositories/ProfileRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.most4dev.recipesapp.domain.repositories
-
-import com.most4dev.recipesapp.domain.entities.ProfileEntity
-
-interface ProfileRepository {
-
-    fun getDataUser(): ProfileEntity
-
-}
